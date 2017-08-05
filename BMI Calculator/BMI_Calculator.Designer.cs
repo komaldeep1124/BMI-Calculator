0c18e7fbd354b92b9dca1157e10f6e7b47bb6f20
rajandhaliwal80 <rdhali36@my.centennialcollege.ca>
2017-08-04 22:57:52 -0400

    Created Result text Box

--- BMI Calculator/BMI_Calculator.Designer.cs
+++ BMI Calculator/BMI_Calculator.Designer.cs
@@ -32,23 +32,39 @@ namespace BMI_Calculator
         /// </summary>
         private void InitializeComponent()
         {
+            this.Result = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // Result
+            // 
+            this.Result.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result.Location = new System.Drawing.Point(1, 60);
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Size = new System.Drawing.Size(300, 61);
+            this.Result.TabIndex = 0;
             // 
             // BMI_Calculator
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Linen;
             this.ClientSize = new System.Drawing.Size(302, 433);
+            this.Controls.Add(this.Result);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.MaximizeBox = false;
             this.Name = "BMI_Calculator";
             this.Text = "BMI";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Result;
     }
 }
 
